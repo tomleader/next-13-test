af679fbb7c65f3a7fe43ae711b35b3ffc62418e8
linktang <tomleader4444@gmail.com>
2025-08-14 19:47:37 +0800

    Update node.ts

--- node-functions/hello/node.ts
+++ node-functions/hello/node.ts
@@ -1,6 +1,6 @@
 export const onRequest = (context) => {
   console.log('121212121212121212121212121');
   // console.log(JSON.stringify(process.env));
-  console.log(JSON.stringify(context));
+  console.log(JSON.stringify(context.server));
   return new Response('Hello, World from node-functions!');
 };
