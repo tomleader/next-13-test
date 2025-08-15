60a6512aab1bd25bee0556a789501690b9e689cc
linktang <tomleader4444@gmail.com>
2025-08-15 14:09:20 +0800

    Update node.ts

--- node-functions/hello/node.ts
+++ node-functions/hello/node.ts
@@ -1,14 +1,32 @@
 export const onRequest = (context) => {
   console.log('121212121212121212121212121');
   // console.log(JSON.stringify(process.env));
-  let s = 'Hello, World from node-functions!';
+  let s = 'Hello, World from node-functions!\n ';
   let arr = Object.keys(context.request);
   let p = process.env;
   console.log(p);
   if (typeof http !== 'undefined') {
-    console.log(http.createServer)
+    // 拦截 HTTP 服务器的请求事件
+    const originalHttpEmit = http.Server.prototype.emit;
+    http.Server.prototype.emit = function (event, req, res) {
+      if (event === 'request') {
+        s = s + req.url;
+        s = s + '\n'
+      }
+      return originalHttpEmit.apply(this, arguments);
+    };
+    
+    // 拦截 HTTPS 服务器的请求事件
+    const originalHttpsEmit = https.Server.prototype.emit;
+    https.Server.prototype.emit = function (event, req, res) {
+      if (event === 'request') {
+        s = s + req.url
+        s = s + '\n'
+      }
+      return originalHttpsEmit.apply(this, arguments);
+    };
   }
-  s = JSON.stringify(p);
+  s = s + JSON.stringify(p);
   //console.log(s);
   return new Response(s);
 };
